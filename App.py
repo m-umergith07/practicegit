e6ec5bfe5a1bd58d68ea3644f7a1bce8563ae16e
Muhammad Umer <hafizumer083@gmail.com>
2026-08-20 18:35:35 +0500

    Subtration & resolving conflicts

--- App.py
+++ App.py
@@ -1,9 +1,14 @@
 def addition(a+b):
     retyurn a+b
 
-def subtraction(a-b):
-    retyurn a-b
+def subtraction(a-b,c):
+    retyurn a-b-c
 
     def division(a,b):
         return a/b
+
+
+
+
+
         
